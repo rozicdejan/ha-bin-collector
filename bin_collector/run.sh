5f248082db4b542b8663c919100d2c00d85440ce
rozicdejan <drozic1989@gmail.com>
2024-11-07 09:12:18 +0100

    Update run.sh

--- bin_collector/run.sh
+++ bin_collector/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Load configuration from config.json
+# Load configuration from options.json
 ADDRESS=$(jq -r '.address' /data/options.json)
 
 # Export environment variable for the Go application
 export ADDRESS
+
+# Ensure the app is executable (not usually needed if set during build)
+chmod +x app
 
 # Run the Go application
 ./app
